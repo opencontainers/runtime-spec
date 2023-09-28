87b3289a9a04bb63c8cc0d1e2493744f02f338b0
Markus Lehtonen <markus.lehtonen@intel.com>
2023-09-28 14:27:20 +0300

    config-linux: add schemata field to IntelRdt
    Add a new "schemata" field to the Linux IntelRdt configuration. This
    addresses the complexity of separate schema fields and resolves the
    issue of supporting currently uncovered RDT features like L2 cache
    allocation and CDP (Code and Data Prioritization).
    
    The new field is for specifying the complete schemata (all schemas) to
    be written to the schemata file in Linux resctrl fs. The aim is for
    simple usage and runtime implementation (by not requiring any
    parsing/filtering of data or otherwise re-implement parsing or
    validation of the Linux resctrl interface) and also to support all RDT
    features now and in the future (i.e. schemas like L2, L2CODE, L2DATA,
    L3CODE and L3DATA and who knows L4 or something else in the future).
    
    Behavior of existing fields is not changed but it is required that the
    new schemata field is applied last.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@intel.com>

--- specs-go/config.go
+++ specs-go/config.go
@@ -795,14 +795,23 @@ type LinuxSyscall struct {
 type LinuxIntelRdt struct {
 	// The identity for RDT Class of Service
 	ClosID string `json:"closID,omitempty"`
+
+	// Schemata specifies the complete schemata to be written as is to the
+	// schemata file in resctrl fs. Each element represents a single line in the schemata file.
+	// NOTE: This will overwrite schemas specified in the L3CacheSchema and/or
+	// MemBwSchema fields.
+	Schemata []string `json:"schemata,omitempty"`
+
 	// The schema for L3 cache id and capacity bitmask (CBM)
 	// Format: "L3:<cache_id0>=<cbm0>;<cache_id1>=<cbm1>;..."
+	// NOTE: Should not be specified if Schemata is non-empty.
 	L3CacheSchema string `json:"l3CacheSchema,omitempty"`
 
 	// The schema of memory bandwidth per L3 cache id
 	// Format: "MB:<cache_id0>=bandwidth0;<cache_id1>=bandwidth1;..."
 	// The unit of memory bandwidth is specified in "percentages" by
 	// default, and in "MBps" if MBA Software Controller is enabled.
+	// NOTE: Should not be specified if Schemata is non-empty.
 	MemBwSchema string `json:"memBwSchema,omitempty"`
 
 	// EnableCMT is the flag to indicate if the Intel RDT CMT is enabled. CMT (Cache Monitoring Technology) supports monitoring of
